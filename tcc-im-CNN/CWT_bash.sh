21b6d5545f790f85349850ac3dd7131685ada88a
Anh Khoa Pham <phamanh@cp2019-02.tnt.uni-hannover.de>
2023-09-16 14:34:28 +0200

    add code cwt tcc

---
+++ tcc-im-CNN/CWT_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Submit the first job
+job_ids=($(sbatch --parsable conwave.sh ))
+echo "First job ID: ${job_ids[0]}"
+
+# Loop through the remaining jobs
+for i in {1..9}
+do
+    # Submit the job with dependency on the previous job, and store the job ID in the array
+    job_ids+=($(sbatch --parsable --dependency=afterok:${job_ids[$i-1]} conwave.sh ))
+    echo "Job ${i} ID: ${job_ids[$i]}"
+done
