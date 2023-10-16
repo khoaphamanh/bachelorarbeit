9c0bc2da7909f9cfe0bd49e8c51f4e84c930b6e5
Anh Khoa Pham <phamanh@cp2019-02.tnt.uni-hannover.de>
2023-10-17 00:35:47 +0200

    change to 100 trials, include CV and training testing

--- ts_im_CNN/GAF_bash.sh
+++ ts_im_CNN/GAF_bash.sh
@@ -5,7 +5,7 @@ job_ids=($(sbatch --parsable gramian.sh ))
 echo "First job ID: ${job_ids[0]}"
 
 # Loop through the remaining jobs
-for i in {1..99}
+for i in {1..100}
 do
     # Submit the job with dependency on the previous job, and store the job ID in the array
     job_ids+=($(sbatch --parsable --dependency=afterok:${job_ids[$i-1]} gramian.sh ))
